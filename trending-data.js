5c1e5e3043ba653d631104d32bb0bba76c46ec14
Eddie Maceda <eddiemaceda11@gmail.com>
2023-02-25 15:57:41 -0500

    Create data for trending cards

---
+++ trending-data.js
@@ -0,0 +1,19 @@
+const trendingData = [
+  {
+    img: "img/trending-tee.avif",
+    name: "Royal 705 Tee Shirt",
+    price: "$40.00 USD",
+  },
+  {
+    img: "img/trending-sweatshirt.avif",
+    name: "Royal 705 Sweatshirt",
+    price: "$50.00 USD",
+  },
+  {
+    img: "img/trending-flask.avif",
+    name: "Royal 705 Hydro Flask",
+    price: "$30.00 USD",
+  },
+];
+
+export { trendingData };
