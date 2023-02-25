4639b86c68b8ec812f35594c3961aec6d748b40d
Eddie Maceda <eddiemaceda11@gmail.com>
2023-02-25 18:11:37 -0500

    Add functionality for gift card section selecting price and updating price display

--- main.js
+++ main.js
@@ -18,3 +18,37 @@ trendingData.map((item) => {
 });
 
 trendingCards.innerHTML = html;
+
+// Fill in background of selected gift card price
+const giftcardPrices = document.querySelector(".amount-choices");
+const gc1 = document.getElementById("gc-1");
+const gc2 = document.getElementById("gc-2");
+const gc3 = document.getElementById("gc-3");
+const gc4 = document.getElementById("gc-4");
+
+giftcardPrices.addEventListener("click", (e) => {
+  if (!e.target.id) {
+    return;
+  } else {
+    giftcardSelection(e.target.id);
+    updateGiftcardPrice(e.target.textContent);
+  }
+});
+
+function giftcardSelection(id) {
+  gc1.classList.remove("gc-price-selected");
+  gc2.classList.remove("gc-price-selected");
+  gc3.classList.remove("gc-price-selected");
+  gc4.classList.remove("gc-price-selected");
+
+  const currentGiftcardChoice = document.getElementById(`${id}`);
+  currentGiftcardChoice.classList.add("gc-price-selected");
+}
+
+// Update gift card price display to match selection
+const displayedGiftcardPrice = document.getElementById("displayed-gc-price");
+
+function updateGiftcardPrice(price) {
+  displayedGiftcardPrice.textContent = "";
+  displayedGiftcardPrice.textContent = `${price} USD`;
+}
